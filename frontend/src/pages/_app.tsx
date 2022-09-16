362410f28ee5a2267fcec6bbe1480f88b05565b5
cotton <bmtosss@gmail.com>
2022-09-16 14:51:14 +0900

    chore: head meta 태그 선언

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Provider } from 'react-redux';
 import { Global, ThemeProvider } from '@emotion/react';
 
@@ -9,12 +10,22 @@ import { theme } from 'src/styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <Global styles={global} />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </Provider>
+    <>
+      <Head>
+        <title>cotton's frontend</title>
+        <meta name="author" content="Beomseok Han" />
+        <meta name="description" content="frontend tech practice page" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="keyword" content="HTML, frontend, tech, cotton" />
+      </Head>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Global styles={global} />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Provider>
+    </>
   );
 }
 
