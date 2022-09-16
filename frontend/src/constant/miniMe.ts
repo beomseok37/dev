34a7b4fd556446f56998a54c8c15ff396b88b73e
cotton <bmtosss@gmail.com>
2022-09-16 16:37:45 +0900

    chore: 상수 선언

---
+++ frontend/src/constant/miniMe.ts
@@ -0,0 +1,36 @@
+const DIRECTION = {
+  FRONT: 0,
+  LEFT: 1,
+  BACK: 2,
+  RIGHT: 3,
+};
+
+const SRC_POSITION = {
+  X: 32,
+  Y: 20,
+};
+
+const SRC_SIZE = {
+  WIDTH: 32,
+  HEIGHT: 40,
+};
+
+const CHARACTER = {
+  WIDTH: 30,
+  HEIGHT: 45,
+};
+const CYCLE_LOOP = [0, 1, 0, 2];
+const MOVEMENT_SPEED = 2;
+const DIRECTION_TERM = 3;
+const FRAME_LIMIT = 12;
+
+export {
+  DIRECTION,
+  SRC_POSITION,
+  SRC_SIZE,
+  CHARACTER,
+  CYCLE_LOOP,
+  MOVEMENT_SPEED,
+  DIRECTION_TERM,
+  FRAME_LIMIT,
+};
