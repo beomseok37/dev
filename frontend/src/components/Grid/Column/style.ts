3d083799aa2ba8d60550d7a405b475a1bcaf0ec9
cotton <bmtosss@gmail.com>
2022-09-20 08:44:29 +0900

    fix: column 구문 오류 수정

--- frontend/src/components/Grid/Column/style.ts
+++ frontend/src/components/Grid/Column/style.ts
@@ -17,12 +17,12 @@ const Wrapper = styled.div<props>`
   flex-direction: column;
 
   width: ${({ width }) => width};
-  height: ${({ height }) => height}
+  height: ${({ height }) => height};
   justify-content: ${({ justifyContent }) => justifyContent};
   align-items: ${({ alignItems }) => alignItems};
   ${({ transition }) => transition && `transition: all ease 0.8s;`}
   background: ${({ theme, background }) => theme.backgroundColors[background]};
-  padding:${({ padding }) => padding};
+  padding: ${({ padding }) => padding};
 `;
 
 // eslint-disable-next-line import/prefer-default-export
