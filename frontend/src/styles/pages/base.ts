fdcb5298738b66434576edf1ce767cd34bac3f82
cotton <bmtosss@gmail.com>
2022-09-29 14:30:57 +0900

    style: base page 스타일 적용

---
+++ frontend/src/styles/pages/base.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const GridWrapper = styled.div`
+  display: grid;
+  padding: 30px;
+  grid-template-columns: 200px 200px 200px 200px;
+  column-gap: 30px;
+  row-gap: 30px;
+  justify-items: center;
+`;
+
+// eslint-disable-next-line import/prefer-default-export
+export { GridWrapper };
