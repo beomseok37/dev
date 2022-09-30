9b9807eada5b544c5271d8d4df5b0d113db66ea3
cotton <bmtosss@gmail.com>
2022-09-30 20:57:30 +0900

    chore: 소켓 연결 위치 이동

--- frontend/src/socket/index.ts
+++ frontend/src/socket/index.ts
@@ -1,5 +1,4 @@
 import { io, Socket } from 'socket.io-client';
-import { DIRECTION } from 'src/constant/miniMe';
 import { ServerToClientType, ClientToServerType } from 'src/types';
 
 const socketURL = process.env.NEXT_PUBLIC_SOCKET_URL;
@@ -10,20 +9,6 @@ const socket: Socket<ServerToClientType, ClientToServerType> = io(socketURL, {
   path: '/socket.io',
   transports: ['websocket'],
   secure: true,
-});
-
-socket.connect();
-
-socket.on('connect', () => {
-  socket.emit('requestConnectedUserInfo', {
-    socketID: socket.id,
-    username: 'me',
-    x: 250,
-    y: 250,
-    character: '/image/character1.png',
-    currentLoopIndex: DIRECTION.FRONT,
-    currentDirection: 0,
-  });
 });
 
 export default socket;
