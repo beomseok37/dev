8a6e23fc941e55a681370a7d149be732b7d625d8
cotton <bmtosss@gmail.com>
2022-09-30 20:57:10 +0900

    chore: position, connectedUser 관련 reducer 제거

--- frontend/src/redux/reducer/index.ts
+++ frontend/src/redux/reducer/index.ts
@@ -2,13 +2,9 @@ import { combineReducers } from '@reduxjs/toolkit';
 import counterReducer from './counter';
 import todoListReducer from './todo';
 import userReducer from './user';
-import connectedUsersReducer from './connectedUsers';
-import positionReducer from './position';
 
 export default combineReducers({
   counter: counterReducer,
   todoList: todoListReducer,
   user: userReducer,
-  connectedUsers: connectedUsersReducer,
-  position: positionReducer,
 });
--- frontend/src/redux/reducer/user.ts
+++ frontend/src/redux/reducer/user.ts
@@ -4,13 +4,7 @@ import type { PayloadAction } from '@reduxjs/toolkit';
 import type { RootState } from 'src/redux/store';
 import { UserType } from 'src/types';
 
-import {
-  CHARACTERURL1,
-  CHARACTERURL2,
-  CHARACTERURL3,
-  CHARACTERURL4,
-  CHARACTERURL5,
-} from 'src/constant/character';
+import { CHARACTERURL1 } from 'src/constant/character';
 
 interface StateType {
   user: UserType;
@@ -27,26 +21,8 @@ export const userSlice = createSlice({
     changeUsername: (state, action: PayloadAction<string>) => {
       state.user.username = action.payload;
     },
-    changeCharacter: (state, action: PayloadAction<number>) => {
-      switch (action.payload) {
-        case 1:
-          state.user.character = CHARACTERURL1;
-          break;
-        case 2:
-          state.user.character = CHARACTERURL2;
-          break;
-        case 3:
-          state.user.character = CHARACTERURL3;
-          break;
-        case 4:
-          state.user.character = CHARACTERURL4;
-          break;
-        case 5:
-          state.user.character = CHARACTERURL5;
-          break;
-        default:
-          break;
-      }
+    changeCharacter: (state, action: PayloadAction<string>) => {
+      state.user.character = action.payload;
     },
   },
 });
