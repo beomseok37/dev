7769a2bdc3b768a783b6504a88421bded2ae3a5c
cotton <bmtosss@gmail.com>
2022-10-01 12:43:52 +0900

    feat: backend 소켓 연결 메소드 수정

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -30,11 +30,11 @@ io.on('connection', (socket) => {
     console.log('fromClientResponse', socketID);
     io.to(socketID).emit('responseConnectedUserInfo', user);
   });
-  socket.on('sendMessage', (socketID, who, message) => {
-    socket.broadcast.emit('broadcastMessage', socketID, who, message);
+  socket.on('sendMessage', (socketID, who, message, character) => {
+    socket.broadcast.emit('broadcastMessage', socketID, who, message, character);
   });
-  socket.on('changeUsername', (socketID, who) => {
-    socket.broadcast.emit('broadcastChangeUsername', socketID, who);
+  socket.on('changeCharacterInfo', (socketID, who, character) => {
+    socket.broadcast.emit('broadcastChangedCharacterInfo', socketID, who, character);
   });
   socket.on('disconnect', () => {
     console.log('disconnect');
