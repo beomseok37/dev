ab660890a8d5d04206a0853ecdd03291bc01ae81
cotton <bmtosss@gmail.com>
2022-10-07 16:31:31 +0900

    feat: sidebar 전체적인 수정

--- frontend/src/components/SideBar/index.tsx
+++ frontend/src/components/SideBar/index.tsx
@@ -1,48 +1,37 @@
 import Link from 'next/link';
-import { ReactElement, useState } from 'react';
-import { GiHamburgerMenu } from 'react-icons/gi';
+import { ReactElement } from 'react';
+import Image from 'next/image';
 
 import Column from 'src/components/Grid/Column';
 
-import {
-  SIDE_BAR_OPEN_WIDTH,
-  SIDE_BAR_CLOSE_WIDTH,
-  SIDE_BAR_HEIGHT,
-} from 'src/constant/sideBar';
+import { SIDE_BAR_OPEN_WIDTH, SIDE_BAR_HEIGHT } from 'src/constant/sideBar';
 
-import { ButtonWrapper, ToggleButton, IconCSS, Anchor } from './style';
+import { Anchor, AnchorWrapper, ImageWrapper, HorizontalLine } from './style';
 
 const menuList = ['home', 'base', 'redux', 'react-hooks', 'animation'];
 
 function SideBar(): ReactElement {
-  const [isOpen, setIsOpen] = useState(true);
-
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <Column
-      width={isOpen ? SIDE_BAR_OPEN_WIDTH : SIDE_BAR_CLOSE_WIDTH}
+      width={SIDE_BAR_OPEN_WIDTH}
       height={SIDE_BAR_HEIGHT}
       alignItems="center"
-      background="#506EA5"
+      background="#fff"
+      borderRight="1px solid #ddd"
+      boxShadow="0 10px 10px #aaa"
       transition
     >
-      <ButtonWrapper>
-        <ToggleButton onClick={handleToggle} isOpen={isOpen}>
-          <GiHamburgerMenu size={24} css={IconCSS} />
-        </ToggleButton>
-      </ButtonWrapper>
-      {isOpen && (
-        <>
-          {menuList.map((menu) => (
-            <Link key={menu} href={menu === 'home' ? '/' : `/${menu}`}>
-              <Anchor>{menu}</Anchor>
-            </Link>
-          ))}
-        </>
-      )}
+      <ImageWrapper>
+        <Image src="/image/road.jpg" width={300} height={300} />
+      </ImageWrapper>
+      <HorizontalLine />
+      {menuList.map((menu) => (
+        <AnchorWrapper key={menu}>
+          <Link href={menu === 'home' ? '/' : `/${menu}`}>
+            <Anchor>{menu}</Anchor>
+          </Link>
+        </AnchorWrapper>
+      ))}
     </Column>
   );
 }
--- frontend/src/components/SideBar/style.ts
+++ frontend/src/components/SideBar/style.ts
@@ -1,62 +1,184 @@
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 
-import {
-  SIDE_BAR_BUTTON_BOX_SHADOW_COLOR,
-  SIDE_BAR_FONT_COLOR,
-  SIDE_BAR_FONT_HOVER_COLOR,
-} from 'src/constant/sideBar';
+// const Anchor1 = styled.button`
+//   width: 100%;
+//   padding: 1rem;
+//   color: ${({ theme }) => theme.fontColors.sideBar};
+//   background: ${({ theme }) => theme.backgroundColors.sideBar};
+//   font-size: 20px;
 
-interface ToggleButtonProps {
-  isOpen: boolean;
-}
+//   &:hover {
+//     cursor: pointer;
+//     color: ${({ theme }) => theme.fontColors.sideBarHover};
+//     background: ${({ theme }) => theme.backgroundColors.sideBarButtonHover};
+//   }
+//   &:active {
+//     box-shadow: 1px 1px 1px 1px ${SIDE_BAR_BUTTON_BOX_SHADOW_COLOR} inset;
+//   }
+// `;
 
-const ButtonWrapper = styled.div`
+// const Anchor2 = styled.button`
+//   width: 100%;
+//   height: 50px;
+//   -webkit-perspective: 230px;
+//   perspective: 300px;
+//   background: transparent;
+//   cursor: pointer;
+//   span {
+//     display: flex;
+//     position: absolute;
+//     justify-content: center;
+//     align-items: center;
+//     width: 100%;
+//     height: 50px;
+
+//     border-radius: 5px;
+//     // background: #0087bf;
+
+//     -webkit-box-sizing: border-box;
+//     -moz-box-sizing: border-box;
+//     box-sizing: border-box;
+
+//     -webkit-transition: all 0.3s;
+//     transition: all 0.3s;
+//   }
+//   span:nth-child(1) {
+//     // box-shadow: -7px -7px 20px 0px #fff9, -4px -4px 5px 0px #fff9,
+//     //   7px 7px 20px 0px #0002, 4px 4px 5px 0px #0001;
+
+//     -webkit-transform: rotateX(90deg);
+//     -moz-transform: rotateX(90deg);
+//     transform: rotateX(90deg);
+//     -webkit-transform-origin: 50% 50% -20px;
+//     -moz-transform-origin: 50% 50% -20px;
+//     transform-origin: 50% 50% -20px;
+//   }
+//   span:nth-child(2) {
+//     // box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+//     //   7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1);
+
+//     -webkit-transform: rotateX(0deg);
+//     -moz-transform: rotateX(0deg);
+//     transform: rotateX(0deg);
+
+//     -webkit-transform-origin: 50% 50% -20px;
+//     -moz-transform-origin: 50% 50% -20px;
+//     transform-origin: 50% 50% -20px;
+//   }
+//   &:hover span:nth-child(1) {
+//     box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+//       7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1);
+
+//     -webkit-transform: rotateX(0deg);
+//     -moz-transform: rotateX(0deg);
+//     transform: rotateX(0deg);
+//   }
+//   &:hover span:nth-child(2) {
+//     // box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+//     //   7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1);
+
+//     -webkit-transform: rotateX(-90deg);
+//     -moz-transform: rotateX(-90deg);
+//     transform: rotateX(-90deg);
+//   }
+// `;
+const animationMoema1 = css`
+  @-webkit-keyframes anim-moema-1 {
+    60% {
+      -webkit-transform: scale3d(0.8, 0.8, 1);
+      transform: scale3d(0.8, 0.8, 1);
+    }
+    85% {
+      -webkit-transform: scale3d(1.1, 1.1, 1);
+      transform: scale3d(1.1, 1.1, 1);
+    }
+    100% {
+      -webkit-transform: scale3d(1, 1, 1);
+      transform: scale3d(1, 1, 1);
+    }
+  }
+  @keyframes anim-moema-1 {
+    60% {
+      -webkit-transform: scale3d(0.8, 0.8, 1);
+      transform: scale3d(0.8, 0.8, 1);
+    }
+    85% {
+      -webkit-transform: scale3d(1.1, 1.1, 1);
+      transform: scale3d(1.1, 1.1, 1);
+    }
+    100% {
+      -webkit-transform: scale3d(1, 1, 1);
+      transform: scale3d(1, 1, 1);
+    }
+  }
+`;
+
+const animationMoema2 = css`
+  @-webkit-keyframes anim-moema-2 {
+    to {
+      opacity: 0;
+      -webkit-transform: scale3d(1, 1, 1);
+      transform: scale3d(1, 1, 1);
+    }
+  }
+  @keyframes anim-moema-2 {
+    to {
+      opacity: 0;
+      -webkit-transform: scale3d(1, 1, 1);
+      transform: scale3d(1, 1, 1);
+    }
+  }
+`;
+
+const AnchorWrapper = styled.div`
+  width: 100%;
+  height: 50px;
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100%;
-  height: 5rem;
 `;
 
-const ToggleButton = styled.button<ToggleButtonProps>`
-  width: 34px;
-  height: 34px;
-  padding: 4px;
-  border-radius: 4px;
-  color: ${({ isOpen }) =>
-    isOpen ? SIDE_BAR_FONT_HOVER_COLOR : SIDE_BAR_FONT_COLOR};
-  background: ${({ theme }) => theme.backgroundColors.sideBarButton};
-  box-shadow: 1px 1px 1px 1px ${SIDE_BAR_BUTTON_BOX_SHADOW_COLOR};
+const Anchor = styled.a`
+  ${animationMoema1}
+  ${animationMoema2}
+
+  width: 240px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  cursor: pointer;
+  color: #444;
   &:hover {
-    cursor: pointer;
+    color: #95b6cf;
+    background-color: #d6e6f5;
+
+    -webkit-transition: background-color 0.1s 0.3s, color 0.1s 0.3s;
+    transition: background-color ease 0.1s 0.3s, color 0.1s 0.3s;
+
+    -webkit-animation: anim-moema-1 0.3s forwards;
+    animation: anim-moema-1 0.3s forwards;
   }
-  &:active {
-    box-shadow: 1px 1px 1px 1px ${SIDE_BAR_BUTTON_BOX_SHADOW_COLOR} inset;
+
+  &:hover::before {
+    -webkit-animation: anim-moema-2 0.3s 0.3s forwards;
+    animation: anim-moema-2 0.3s 0.3s forwards;
   }
 `;
 
-const IconCSS = css`
-  &:hover {
-    transform: rotate(0.1turn);
-  }
-  transition: all ease 0.5s;
+const ImageWrapper = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 100px;
+  background: #fff;
+  overflow: hidden;
 `;
 
-const Anchor = styled.button`
-  width: 100%;
-  padding: 1rem;
-  color: ${({ theme }) => theme.fontColors.sideBar};
-  background: ${({ theme }) => theme.backgroundColors.sideBar};
-  font-size: 20px;
-
-  &:hover {
-    cursor: pointer;
-    color: ${({ theme }) => theme.fontColors.sideBarHover};
-    background: ${({ theme }) => theme.backgroundColors.sideBarButtonHover};
-  }
-  &:active {
-    box-shadow: 1px 1px 1px 1px ${SIDE_BAR_BUTTON_BOX_SHADOW_COLOR} inset;
-  }
+const HorizontalLine = styled.hr`
+  width: 200px;
+  border: thin solid #ddd;
 `;
-export { ButtonWrapper, ToggleButton, IconCSS, Anchor };
+
+export { Anchor, AnchorWrapper, ImageWrapper, HorizontalLine };
