68f773d1f15a238040195a42aad0c85792f5f742
cotton <bmtosss@gmail.com>
2022-10-09 17:11:29 +0900

    feat: sidebar 수정 (완)

--- frontend/src/components/SideBar/index.tsx
+++ frontend/src/components/SideBar/index.tsx
@@ -1,12 +1,23 @@
 import Link from 'next/link';
 import { ReactElement } from 'react';
 import Image from 'next/image';
+import { IoLogoGithub } from 'react-icons/io';
+import { IoMailOutline } from 'react-icons/io5';
 
 import Column from 'src/components/Grid/Column';
+import Row from 'src/components/Grid/Row';
 
 import { SIDE_BAR_OPEN_WIDTH, SIDE_BAR_HEIGHT } from 'src/constant/sideBar';
 
-import { Anchor, AnchorWrapper, ImageWrapper, HorizontalLine } from './style';
+import {
+  Anchor2,
+  // AnchorWrapper,
+  ImageWrapper,
+  HorizontalLine,
+  BlogTitle,
+  BlackBackground,
+  IconWrapper,
+} from './style';
 
 const menuList = ['home', 'base', 'redux', 'react-hooks', 'animation'];
 
@@ -24,13 +35,53 @@ function SideBar(): ReactElement {
       <ImageWrapper>
         <Image src="/image/road.jpg" width={300} height={300} />
       </ImageWrapper>
+      <BlogTitle>Cotton dev</BlogTitle>
+      <HorizontalLine />
+      <Row
+        width="100%"
+        height="40px"
+        justifyContent="space-evenly"
+        alignItems="center"
+      >
+        <Link href="https://github.com/beomseok37">
+          <a target="_blank">
+            <IconWrapper>
+              <IoLogoGithub size={32} color="#000" />
+            </IconWrapper>
+          </a>
+        </Link>
+        <Link href="https://blog.beomseok.dev">
+          <a target="_blank">
+            <IconWrapper>
+              <Image src="/image/logomark.svg" width={37} height={37} />
+            </IconWrapper>
+          </a>
+        </Link>
+        <Link href="mailto:bmtosss@gmail.com">
+          <a target="_blank">
+            <IconWrapper>
+              <BlackBackground>
+                <IoMailOutline size={20} color="#fff" />
+              </BlackBackground>
+            </IconWrapper>
+          </a>
+        </Link>
+      </Row>
       <HorizontalLine />
       {menuList.map((menu) => (
-        <AnchorWrapper key={menu}>
-          <Link href={menu === 'home' ? '/' : `/${menu}`}>
-            <Anchor>{menu}</Anchor>
-          </Link>
-        </AnchorWrapper>
+        <Link key={menu} href={menu === 'home' ? '/' : `/${menu}`}>
+          <Anchor2>
+            <span>{menu}</span>
+            <span>{menu}</span>
+          </Anchor2>
+        </Link>
+        // <AnchorWrapper key={menu}>
+        // <Link href={menu === 'home' ? '/' : `/${menu}`}>
+        //   <Anchor>
+        //     {menu}
+        //   </Anchor>
+        // </Link>
+        // </AnchorWrapper>
       ))}
     </Column>
   );
--- frontend/src/components/SideBar/style.ts
+++ frontend/src/components/SideBar/style.ts
@@ -18,148 +18,146 @@ import { css } from '@emotion/react';
 //   }
 // `;
 
-// const Anchor2 = styled.button`
-//   width: 100%;
-//   height: 50px;
-//   -webkit-perspective: 230px;
-//   perspective: 300px;
-//   background: transparent;
-//   cursor: pointer;
-//   span {
-//     display: flex;
-//     position: absolute;
-//     justify-content: center;
-//     align-items: center;
-//     width: 100%;
-//     height: 50px;
-
-//     border-radius: 5px;
-
-//     -webkit-box-sizing: border-box;
-//     -moz-box-sizing: border-box;
-//     box-sizing: border-box;
-
-//     -webkit-transition: all 0.3s;
-//     transition: all 0.3s;
-//   }
-//   span:nth-child(1) {
-
-//     -webkit-transform: rotateX(90deg);
-//     -moz-transform: rotateX(90deg);
-//     transform: rotateX(90deg);
-//     -webkit-transform-origin: 50% 50% -20px;
-//     -moz-transform-origin: 50% 50% -20px;
-//     transform-origin: 50% 50% -20px;
-//   }
-//   span:nth-child(2) {
-
-//     -webkit-transform: rotateX(0deg);
-//     -moz-transform: rotateX(0deg);
-//     transform: rotateX(0deg);
-
-//     -webkit-transform-origin: 50% 50% -20px;
-//     -moz-transform-origin: 50% 50% -20px;
-//     transform-origin: 50% 50% -20px;
-//   }
-//   &:hover span:nth-child(1) {
-//     box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
-//       7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1);
-
-//     -webkit-transform: rotateX(0deg);
-//     -moz-transform: rotateX(0deg);
-//     transform: rotateX(0deg);
-//   }
-//   &:hover span:nth-child(2) {
-
-//     -webkit-transform: rotateX(-90deg);
-//     -moz-transform: rotateX(-90deg);
-//     transform: rotateX(-90deg);
-//   }
-// `;
-const animationMoema1 = css`
-  @-webkit-keyframes anim-moema-1 {
-    60% {
-      -webkit-transform: scale3d(0.8, 0.8, 1);
-      transform: scale3d(0.8, 0.8, 1);
-    }
-    85% {
-      -webkit-transform: scale3d(1.1, 1.1, 1);
-      transform: scale3d(1.1, 1.1, 1);
-    }
-    100% {
-      -webkit-transform: scale3d(1, 1, 1);
-      transform: scale3d(1, 1, 1);
-    }
-  }
-  @keyframes anim-moema-1 {
-    60% {
-      -webkit-transform: scale3d(0.8, 0.8, 1);
-      transform: scale3d(0.8, 0.8, 1);
-    }
-    85% {
-      -webkit-transform: scale3d(1.1, 1.1, 1);
-      transform: scale3d(1.1, 1.1, 1);
-    }
-    100% {
-      -webkit-transform: scale3d(1, 1, 1);
-      transform: scale3d(1, 1, 1);
-    }
-  }
-`;
-
-const animationMoema2 = css`
-  @-webkit-keyframes anim-moema-2 {
-    to {
-      opacity: 0;
-      -webkit-transform: scale3d(1, 1, 1);
-      transform: scale3d(1, 1, 1);
-    }
-  }
-  @keyframes anim-moema-2 {
-    to {
-      opacity: 0;
-      -webkit-transform: scale3d(1, 1, 1);
-      transform: scale3d(1, 1, 1);
-    }
-  }
-`;
-
-const AnchorWrapper = styled.div`
+const Anchor2 = styled.button`
   width: 100%;
   height: 50px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Anchor = styled.a`
-  ${animationMoema1}
-  ${animationMoema2}
-
-  width: 240px;
-  height: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 5px;
+  -webkit-perspective: 230px;
+  perspective: 300px;
+  background: transparent;
   cursor: pointer;
-  color: #444;
-  &:hover {
-    color: #95b6cf;
-    background-color: #d6e6f5;
+  span {
+    display: flex;
+    position: absolute;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 50px;
 
-    -webkit-transition: background-color 0.1s 0.3s, color 0.1s 0.3s;
-    transition: background-color ease 0.1s 0.3s, color 0.1s 0.3s;
+    border-radius: 5px;
 
-    -webkit-animation: anim-moema-1 0.3s forwards;
-    animation: anim-moema-1 0.3s forwards;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+
+    -webkit-transition: all 0.3s;
+    transition: all 0.3s;
   }
+  span:nth-child(1) {
+    -webkit-transform: rotateX(90deg);
+    -moz-transform: rotateX(90deg);
+    transform: rotateX(90deg);
+    -webkit-transform-origin: 50% 50% -20px;
+    -moz-transform-origin: 50% 50% -20px;
+    transform-origin: 50% 50% -20px;
+  }
+  span:nth-child(2) {
+    -webkit-transform: rotateX(0deg);
+    -moz-transform: rotateX(0deg);
+    transform: rotateX(0deg);
 
-  &:hover::before {
-    -webkit-animation: anim-moema-2 0.3s 0.3s forwards;
-    animation: anim-moema-2 0.3s 0.3s forwards;
+    -webkit-transform-origin: 50% 50% -20px;
+    -moz-transform-origin: 50% 50% -20px;
+    transform-origin: 50% 50% -20px;
+  }
+  &:hover span:nth-child(1) {
+    box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+      7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1);
+
+    -webkit-transform: rotateX(0deg);
+    -moz-transform: rotateX(0deg);
+    transform: rotateX(0deg);
+  }
+  &:hover span:nth-child(2) {
+    -webkit-transform: rotateX(-90deg);
+    -moz-transform: rotateX(-90deg);
+    transform: rotateX(-90deg);
   }
 `;
+// const animationMoema1 = css`
+//   @-webkit-keyframes anim-moema-1 {
+//     60% {
+//       -webkit-transform: scale3d(0.8, 0.8, 1);
+//       transform: scale3d(0.8, 0.8, 1);
+//     }
+//     85% {
+//       -webkit-transform: scale3d(1.1, 1.1, 1);
+//       transform: scale3d(1.1, 1.1, 1);
+//     }
+//     100% {
+//       -webkit-transform: scale3d(1, 1, 1);
+//       transform: scale3d(1, 1, 1);
+//     }
+//   }
+//   @keyframes anim-moema-1 {
+//     60% {
+//       -webkit-transform: scale3d(0.8, 0.8, 1);
+//       transform: scale3d(0.8, 0.8, 1);
+//     }
+//     85% {
+//       -webkit-transform: scale3d(1.1, 1.1, 1);
+//       transform: scale3d(1.1, 1.1, 1);
+//     }
+//     100% {
+//       -webkit-transform: scale3d(1, 1, 1);
+//       transform: scale3d(1, 1, 1);
+//     }
+//   }
+// `;
+
+// const animationMoema2 = css`
+//   @-webkit-keyframes anim-moema-2 {
+//     to {
+//       opacity: 0;
+//       -webkit-transform: scale3d(1, 1, 1);
+//       transform: scale3d(1, 1, 1);
+//     }
+//   }
+//   @keyframes anim-moema-2 {
+//     to {
+//       opacity: 0;
+//       -webkit-transform: scale3d(1, 1, 1);
+//       transform: scale3d(1, 1, 1);
+//     }
+//   }
+// `;
+
+// const AnchorWrapper = styled.div`
+//   width: 100%;
+//   height: 50px;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `;
+
+// const Anchor = styled.a`
+//   ${animationMoema1}
+//   ${animationMoema2}
+
+//   width: 240px;
+//   height: 40px;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   border-radius: 5px;
+//   cursor: pointer;
+//   color: #444;
+//   &:hover {
+//     // color: #fff;
+//     background-color: #ccc;
+//     width: 200px;
+
+//     -webkit-transition: background-color 0.1s 0.3s, color 0.1s 0.3s;
+//     transition: background-color ease 0.1s 0.3s, color 0.1s 0.3s;
+
+//     -webkit-animation: anim-moema-1 0.3s forwards;
+//     animation: anim-moema-1 0.3s forwards;
+//   }
+
+//   &:hover::before {
+//     -webkit-animation: anim-moema-2 0.3s 0.3s forwards;
+//     animation: anim-moema-2 0.3s 0.3s forwards;
+//   }
+// `;
 
 const ImageWrapper = styled.div`
   width: 200px;
@@ -167,6 +165,7 @@ const ImageWrapper = styled.div`
   border-radius: 100px;
   background: #fff;
   overflow: hidden;
+  margin-top: 10px;
 `;
 
 const HorizontalLine = styled.hr`
@@ -174,4 +173,54 @@ const HorizontalLine = styled.hr`
   border: thin solid #ddd;
 `;
 
-export { Anchor, AnchorWrapper, ImageWrapper, HorizontalLine };
+const BlogTitle = styled.p`
+  font-size: 20px;
+  font-weight: bold;
+  margin-top: 10px;
+`;
+
+const BlackBackground = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 28px;
+  height: 28px;
+  border-radius: 20px;
+  background: #000;
+`;
+
+const waveAnimation = css`
+  @keyframes wave {
+    0% {
+      transform: scale(1);
+      // opacity: 1;
+    }
+
+    50% {
+      transform: scale(1.1);
+      // opacity: 0;
+    }
+
+    100% {
+      transform: scale(1);
+      // opacity: 1;
+    }
+  }
+`;
+
+const IconWrapper = styled.div`
+  ${waveAnimation}
+  &:hover {
+    animation: wave 1s infinite linear;
+  }
+`;
+
+export {
+  Anchor2,
+  // AnchorWrapper,
+  ImageWrapper,
+  HorizontalLine,
+  BlogTitle,
+  BlackBackground,
+  IconWrapper,
+};
