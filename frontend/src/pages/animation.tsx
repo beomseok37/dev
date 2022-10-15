f312a533536db48edacca8f0679077122223e87e
cotton <bmtosss@gmail.com>
2022-10-15 22:13:23 +0900

    chore: 오타 수정

--- frontend/src/pages/animation.tsx
+++ frontend/src/pages/animation.tsx
@@ -3,13 +3,13 @@ import { NextPage } from 'next';
 import Page from 'src/components/Page';
 import PracticeAnimation from 'src/components/PracticeAnimation';
 
-import { ANIMIATION_PAGE_CONTENT } from 'src/constant/page';
+import { ANIMATION_PAGE_CONTENT } from 'src/constant/page';
 
 const Animation: NextPage = () => {
   return (
     <Page
       header="animation"
-      pageContentList={[{ content: ANIMIATION_PAGE_CONTENT, done: true }]}
+      pageContentList={[{ content: ANIMATION_PAGE_CONTENT, done: true }]}
     >
       <PracticeAnimation />
     </Page>
