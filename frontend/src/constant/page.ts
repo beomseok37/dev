8e859b38b6bf73bfd8d37f90cde44a914a252a7f
cotton <bmtosss@gmail.com>
2022-10-16 10:47:18 +0900

    chore: page content 수정

--- frontend/src/constant/page.ts
+++ frontend/src/constant/page.ts
@@ -5,7 +5,7 @@ js(emotion), mixin 연습`;
 const REDUX_COUNT_CONTENT = `redux를 이용해 counter 만들기`;
 const REDUX_TODO_LIST_CONTENT = `redux를 이용해 todo list 만들기`;
 const REACT_HOOKS_PAGE_CONTENT = `react-hooks인 useMemo와 useCallback을 공부하는 페이지입니다.`;
-const ANIMATION_PAGE_CONTENT = `transform, translate 공부`;
+const ANIMATION_PAGE_CONTENT = `transform, translate 등 animation css 공부`;
 const DROPDOWN_MENU_PAGE_CONTENT = `dropdown list 만들어보기`;
 const BASE_PAGE_CONTENT = `여러 컴포넌트들을 시도해보는 페이지 입니다.`;
 const ECHARTS_PAGE_CONTENT = `echarts 패키지의 막대 그래프를 연습해보는 페이지 입니다.`;
