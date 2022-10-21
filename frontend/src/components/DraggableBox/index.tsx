1f668302e8347ae51572a76cf642f4269a17cb0f
cotton <bmtosss@gmail.com>
2022-10-21 15:13:07 +0900

    feat: xarrows관련 컴포넌트 구현
    - DraggableBox
    - ParameterSetModal

---
+++ frontend/src/components/DraggableBox/index.tsx
@@ -0,0 +1,54 @@
+/* eslint-disable no-unused-vars */
+import { ReactElement, MouseEvent, useState } from 'react';
+import Draggable from 'react-draggable';
+import { useXarrow } from 'react-xarrows';
+import { GrPowerReset } from 'react-icons/gr';
+
+import ParameterSetModal from 'src/components/modals/ParamterSetModal';
+
+import { ComponentType } from 'src/types';
+
+import { Box, Button } from './style';
+
+interface Props {
+  component: ComponentType;
+  onClick: (e: MouseEvent<HTMLDivElement>) => void;
+  onSaveParameter: (id: string, parameter1: string, parameter2: string) => void;
+}
+
+function DraggableBox({
+  component,
+  onClick,
+  onSaveParameter,
+}: Props): ReactElement {
+  const updateXarrow = useXarrow();
+  const [modalShow, setModalShow] = useState(false);
+
+  const handleModalShow = () => {
+    setModalShow(true);
+  };
+  const handleClose = () => {
+    setModalShow(false);
+  };
+  return (
+    <>
+      <Draggable onDrag={updateXarrow} onStop={updateXarrow}>
+        <Box id={component.id} onClick={onClick}>
+          {component.id}
+          <Button onClick={handleModalShow}>
+            <GrPowerReset />
+          </Button>
+        </Box>
+      </Draggable>
+      {modalShow && (
+        <ParameterSetModal
+          onClose={handleClose}
+          component={component}
+          onSaveParameter={onSaveParameter}
+        />
+      )}
+    </>
+  );
+}
+
+export default DraggableBox;
---
+++ frontend/src/components/modals/ParamterSetModal/index.tsx
@@ -0,0 +1,59 @@
+import { ChangeEvent, ReactElement, useState } from 'react';
+
+import Column from 'src/components/Grid/Column';
+import Row from 'src/components/Grid/Row';
+import Input from 'src/components/base/Input';
+import ModalCommon from 'src/components/modals/Common';
+
+import { ComponentType } from 'src/types';
+
+import { Button } from './style';
+
+interface Props {
+  onClose: () => void;
+  component: ComponentType;
+  // eslint-disable-next-line no-unused-vars
+  onSaveParameter: (id: string, parameter1: string, parameter2: string) => void;
+}
+function ParameterSetModal({
+  onClose,
+  component,
+  onSaveParameter,
+}: Props): ReactElement {
+  const [param1, setParam1] = useState(component.parameter.parameter1);
+  const [param2, setParam2] = useState(component.parameter.parameter2);
+
+  const handleChange1 = (e: ChangeEvent<HTMLInputElement>) => {
+    setParam1(e.currentTarget.value);
+  };
+  const handleChange2 = (e: ChangeEvent<HTMLInputElement>) => {
+    setParam2(e.currentTarget.value);
+  };
+  const handleClickSave = () => {
+    onSaveParameter(component.id, param1, param2);
+    onClose();
+  };
+  return (
+    <ModalCommon width={500} height={300} onClose={onClose}>
+      <Column
+        width="100%"
+        height="100%"
+        justifyContent="space-evenly"
+        alignItems="center"
+      >
+        <p>{component.id}'s parameter</p>
+        <Row width="100%" justifyContent="space-around" alignItems="center">
+          <p>param1</p>
+          <Input onChange={handleChange1} value={param1} width="300px" />
+        </Row>
+        <Row width="100%" justifyContent="space-around" alignItems="center">
+          <p>param2</p>
+          <Input onChange={handleChange2} value={param2} width="300px" />
+        </Row>
+        <Button onClick={handleClickSave}>parameter save</Button>
+      </Column>
+    </ModalCommon>
+  );
+}
+
+export default ParameterSetModal;
