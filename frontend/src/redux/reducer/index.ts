76f94b700dfde2f8574871705204e8ef6ee0830a
cotton <bmtosss@gmail.com>
2022-10-24 16:31:14 +0900

    chore: 전역 상태 이름 변경

--- frontend/src/redux/reducer/index.ts
+++ frontend/src/redux/reducer/index.ts
@@ -2,11 +2,11 @@ import { combineReducers } from '@reduxjs/toolkit';
 import counterReducer from './counter';
 import todoListReducer from './todo';
 import userReducer from './user';
-import chatReducer from './chat';
+import wholeChatReducer from './chat';
 
 export default combineReducers({
   counter: counterReducer,
   todoList: todoListReducer,
   user: userReducer,
-  chat: chatReducer,
+  wholeChat: wholeChatReducer,
 });
