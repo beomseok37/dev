a95ccfbb5483e4e9e1799fbdc7a6fe4b38b0e557
cotton <bmtosss@gmail.com>
2022-10-29 16:49:37 +0900

    feat: github repo api 연결

--- frontend/src/pages/study/github-api.tsx
+++ frontend/src/pages/study/github-api.tsx
@@ -1,33 +1,93 @@
 /* eslint-disable react/no-danger */
 import { NextPage } from 'next';
+import { useState, ChangeEvent, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import * as DOMPurify from 'dompurify';
 
 import Page from 'src/components/Page';
+import Column from 'src/components/Grid/Column';
+import Row from 'src/components/Grid/Row';
+import Input from 'src/components/base/Input';
+import Button from 'src/components/base/Button';
+import DropdownMenu from 'src/components/DropdownMenu';
 
 import { GITHUB_API_PAGE_CONTENT } from 'src/constant/page';
-import { useState, useEffect } from 'react';
 
 const serverURL = process.env.NEXT_PUBLIC_SERVER_URL;
 
 const GitHubApiPage: NextPage = () => {
   const [readme, setReadme] = useState('');
-  const getData = async () => {
-    const readmeData = await axios.get(`${serverURL}/v1/readme/beomseok37`);
-    setReadme(readmeData.data.data);
+  const [username, setUsername] = useState('');
+  const [repoList, setRepoList] = useState<string[]>([]);
+  const [repo, setRepo] = useState('');
+  const getRepoList = async () => {
+    const fetchedRepoList = await axios.get(
+      `${serverURL}/v1/github/repos/${username}`
+    );
+    setRepoList(fetchedRepoList.data.data.map((data: any) => data.name));
+  };
+
+  const getReadme = useCallback(async () => {
+    const fetchedReadme = await axios.get(
+      `${serverURL}/v1/github/${username}/${repo}`
+    );
+    setReadme(fetchedReadme.data.data);
+  }, [repo, username]);
+
+  const handleChangeUsername = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.currentTarget.value);
+  };
+  const handleChangeRepo = (e: ChangeEvent<HTMLInputElement>) => {
+    setRepo(e.currentTarget.value);
   };
 
   useEffect(() => {
-    getData();
-  }, []);
+    if (repo) {
+      getReadme();
+    }
+  }, [getReadme, repo]);
+
   return (
     <Page
-      header="animation"
+      header="github-api"
       pageContentList={[{ content: GITHUB_API_PAGE_CONTENT, done: true }]}
     >
-      {readme && (
-        <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(readme) }} />
-      )}
+      <Column padding="10px">
+        <Row padding="20px" height="300px">
+          <Column>
+            <Row
+              alignItems="center"
+              justifyContent="space-between"
+              width="200px"
+            >
+              <p>username</p>
+              <Input value={username} onChange={handleChangeUsername} />
+            </Row>
+            <Row
+              alignItems="center"
+              justifyContent="space-between"
+              width="200px"
+            >
+              <p>repository</p>
+              <Input value={repo} onChange={handleChangeRepo} />
+            </Row>
+            <Button onClick={() => getRepoList()}>찾기</Button>
+          </Column>
+          {repoList.length !== 0 && (
+            <DropdownMenu
+              bind={[repo, setRepo]}
+              menuList={repoList}
+              version={1}
+            />
+          )}
+        </Row>
+
+        {readme && (
+          <div
+            dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(readme) }}
+          />
+        )}
+      </Column>
     </Page>
   );
 };
