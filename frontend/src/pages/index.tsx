532d824e8abf740883f4caf92ec9e165ca2718ec
cotton <bmtosss@gmail.com>
2022-11-04 07:56:56 +0900

    feat: github 잔디 component 추가

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,20 @@
 import type { NextPage } from 'next';
-import { useEffect } from 'react';
+import { useEffect, Suspense } from 'react';
 import { useDispatch } from 'react-redux';
+import dynamic from 'next/dynamic';
 
 import Page from 'src/components/Page';
+import Row from 'src/components/Grid/Row';
 import WholeChat from 'src/components/chat/WholeChat';
+import Loading from 'src/components/Loading';
 
 import { chatIn, changeUserInfoInChat } from 'src/redux/reducer/chat';
 
 import socket from 'src/socket';
+
+const ReactGitHubCalendar = dynamic(() => import('react-ts-github-calendar'), {
+  ssr: false,
+});
 
 const Home: NextPage = () => {
   const dispatch = useDispatch();
@@ -30,6 +37,15 @@ const Home: NextPage = () => {
       <p style={{ padding: '0 0 0 10px' }}>
         이 블로그는 frontend의 여러 기술들을 연습하기 위한 사이트이다.
       </p>
+      <Row padding="20px 0 0 20px">
+        <Suspense fallback={<Loading />}>
+          <ReactGitHubCalendar
+            userName="beomseok37"
+            global_stats={false}
+            tooltips
+          />
+        </Suspense>
+      </Row>
       <WholeChat />
     </Page>
   );
