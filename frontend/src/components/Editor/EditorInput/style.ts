4bccebce361b02553eb8d1807bde1a09a94523ae
cotton <bmtosss@gmail.com>
2022-11-03 15:06:01 +0900

    style: textarea font 수정

--- frontend/src/components/Editor/EditorInput/style.ts
+++ frontend/src/components/Editor/EditorInput/style.ts
@@ -6,6 +6,9 @@ const TextArea = styled.textarea`
   outline: none;
   padding: 10px;
   margin-right: 20px;
+  font-size: 16px;
+  font-weight: 700;
+  font-family: inherit;
 `;
 
 // eslint-disable-next-line import/prefer-default-export
