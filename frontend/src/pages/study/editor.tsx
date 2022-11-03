a6411e73e354e20e76ea0c2bfc811d158e2b3615
cotton <bmtosss@gmail.com>
2022-11-03 16:16:02 +0900

    chore: editor page 설명 추가

--- frontend/src/pages/study/editor.tsx
+++ frontend/src/pages/study/editor.tsx
@@ -19,7 +19,11 @@ const EditorPage: NextPage = () => {
       {/* <Suspense fallback={<Loading />}>
           <DynamicEditor />
         </Suspense> */}
-      😥
+      <p>
+        😥수식 편집기 구현은 완료하였으나 react 관련 패키지라 작동하지 않아 주석
+        처리했습니다.
+      </p>
+      <p>local에서 실행할 경우 editor 페이지의 주석을 되돌리면 동작합니다.</p>
     </Page>
   );
 };
