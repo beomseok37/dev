430a7a18364c41ffe185c0bc6e555762ba5cd65a
cotton <bmtosss@gmail.com>
2022-11-04 07:57:23 +0900

    fix: next에서 window is not defined 오류나는 패키지 관련 수정

--- frontend/src/pages/study/editor.tsx
+++ frontend/src/pages/study/editor.tsx
@@ -1,14 +1,15 @@
 import { NextPage } from 'next';
-// import dynamic from 'next/dynamic';
-// import Script from 'next/script';
-// import { Suspense } from 'react';
+import dynamic from 'next/dynamic';
+import { Suspense } from 'react';
 
 import Page from 'src/components/Page';
-// import Loading from 'src/components/Loading';
+import Loading from 'src/components/Loading';
 
 import { EDITOR_PAGE_CONTENT } from 'src/constant/page';
 
-// const DynamicEditor = dynamic(() => import('src/components/Editor'));
+const DynamicEditor = dynamic(() => import('src/components/Editor'), {
+  ssr: false,
+});
 
 const EditorPage: NextPage = () => {
   return (
@@ -16,14 +17,9 @@ const EditorPage: NextPage = () => {
       header="equation editor"
       pageContentList={[{ content: EDITOR_PAGE_CONTENT, done: true }]}
     >
-      {/* <Suspense fallback={<Loading />}>
-          <DynamicEditor />
-        </Suspense> */}
-      <p>
-        😥수식 편집기 구현은 완료하였으나 react 관련 패키지라 작동하지 않아 주석
-        처리했습니다.
-      </p>
-      <p>local에서 실행할 경우 editor 페이지의 주석을 되돌리면 동작합니다.</p>
+      <Suspense fallback={<Loading />}>
+        <DynamicEditor />
+      </Suspense>
     </Page>
   );
 };
