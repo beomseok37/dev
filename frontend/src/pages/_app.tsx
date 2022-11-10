3e744490f88434b8d9d20c8b3a8d99095eed0c34
cotton <bmtosss@gmail.com>
2022-11-10 13:18:44 +0900

    refactor: _document 페이지 선언
    - head 태그 이동

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 import { Provider } from 'react-redux';
 import { Global, ThemeProvider } from '@emotion/react';
 
@@ -7,17 +6,13 @@ import store from 'src/redux/store';
 
 import { global } from 'src/styles/global';
 import { theme } from 'src/styles/theme';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>cotton's frontend</title>
-        <meta name="author" content="Beomseok Han" />
-        <meta name="description" content="frontend tech practice page" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="keyword" content="HTML, frontend, tech, cotton" />
+        <title>Cotton's frontend</title>
       </Head>
       <Provider store={store}>
         <ThemeProvider theme={theme}>
