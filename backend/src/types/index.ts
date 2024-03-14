cbe67d0c586ce2744a9ef002cd34a83b4f882406
beomseok <bmtosss@gmail.com>
2024-03-15 00:07:42 +0900

    backend 수정

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,5 +1,5 @@
 import ServerToClientType from './serverToClient';
 import ClientToServerType from './clientToServer';
-import { SocketUserInfoType, SocketChatType } from './socketUserInfo';
+import SocketUserInfoType from './socketUserInfo';
 
-export { ServerToClientType, ClientToServerType, SocketUserInfoType, SocketChatType };
+export { ServerToClientType, ClientToServerType, SocketUserInfoType };
